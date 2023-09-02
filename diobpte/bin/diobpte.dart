34403334b5b0dc1bec437a2f00da6a5dfd5a57e2
maurojh <maurojh@gmail.com>
2023-09-02 14:53:15 -0300

    Finally

--- diobpte/bin/diobpte.dart
+++ diobpte/bin/diobpte.dart
@@ -6,6 +6,13 @@ import 'dart:io';
 void main(List<String> arguments) {
   print('Digite um número:');
   var linha = stdin.readLineSync(encoding: utf8);
-  double numero = double.parse(linha ?? '');
-  print(numero);
+  try {
+    double numero = double.parse(linha ?? '');
+    print(numero);
+  } catch (e) {
+    print('Número inválido: $linha');
+  } finally {
+    print('Executando finally');
+  }
+  
 }
